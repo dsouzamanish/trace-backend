f5118ba1af9b2cc45f82e9b0236990af291e526e
dsouzamanish <dsouzamanish@gmail.com>
2026-01-07 15:49:51 +0530

    fix: add missing ContentstackModule import to TeamMemberModule

--- src/modules/team-member/team-member.module.ts
+++ src/modules/team-member/team-member.module.ts
@@ -2,9 +2,10 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TeamMemberService } from './team-member.service';
 import { TeamMemberController } from './team-member.controller';
 import { TeamModule } from '../team/team.module';
+import { ContentstackModule } from '../contentstack/contentstack.module';
 
 @Module({
-  imports: [forwardRef(() => TeamModule)],
+  imports: [ContentstackModule, forwardRef(() => TeamModule)],
   controllers: [TeamMemberController],
   providers: [TeamMemberService],
   exports: [TeamMemberService],
